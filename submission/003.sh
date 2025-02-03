a803c1824b4e67bb6d94ff5f009ab593192082d6
Gabriel de Melo Filipe <melogamefi@gmail.com>
2025-02-02 23:34:38 -0300

    Envio da questão 03: Quantidade de novos outputs no bloco 123,456

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,13 @@
 # How many new outputs were created by block 123,456?
+#!/bin/bash
+
+block_hash="0000000000002917ed80650c6174aac8dfc46f5fe36480aaef682ff6cd83c3ca"
+txs=$(bitcoin-cli getblock $block_hash | jq -r '.tx[]')
+output_count=0
+
+for tx in $txs; do
+    count=$(bitcoin-cli getrawtransaction $tx true | jq '.vout | length')
+    output_count=$((output_count + count))
+done
+
+echo "$output_count"
